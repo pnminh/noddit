6fdbb264b75e1a3ff16cb369e4357fe4aceb8d09
minhpham <minh.pham@dhigroupinc.com>
2018-12-02 11:45:43 -0600

    use different dml for different env

--- src/db/migration/1543364915973-Initialize.ts
+++ src/db/migration/1543364915973-Initialize.ts
@@ -1,11 +1,12 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
-
+const env = process.env.NODE_ENV || 'development';
+const autoIncrement = env==='production'?"SERIAL":"AUTOINCREMENT";
 export class Initialize1543364915973 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`CREATE TABLE "topic" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "title" varchar NOT NULL, "description" varchar NOT NULL)`);
-        await queryRunner.query(`CREATE TABLE "banner" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "source" varchar NOT NULL, "description" varchar NOT NULL, "topicId" integer)`);
-        await queryRunner.query(`CREATE TABLE "temporary_banner" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "source" varchar NOT NULL, "description" varchar NOT NULL, "topicId" integer, CONSTRAINT "FK_218d44aa86e285ebd6a2f34950b" FOREIGN KEY ("topicId") REFERENCES "topic" ("id"))`);
+        await queryRunner.query(`CREATE TABLE "topic" ("id" integer PRIMARY KEY ${autoIncrement} NOT NULL, "title" varchar NOT NULL, "description" varchar NOT NULL)`);
+        await queryRunner.query(`CREATE TABLE "banner" ("id" integer PRIMARY KEY ${autoIncrement} NOT NULL, "source" varchar NOT NULL, "description" varchar NOT NULL, "topicId" integer)`);
+        await queryRunner.query(`CREATE TABLE "temporary_banner" ("id" integer PRIMARY KEY ${autoIncrement} NOT NULL, "source" varchar NOT NULL, "description" varchar NOT NULL, "topicId" integer, CONSTRAINT "FK_218d44aa86e285ebd6a2f34950b" FOREIGN KEY ("topicId") REFERENCES "topic" ("id"))`);
         await queryRunner.query(`INSERT INTO "temporary_banner"("id", "source", "description", "topicId") SELECT "id", "source", "description", "topicId" FROM "banner"`);
         await queryRunner.query(`DROP TABLE "banner"`);
         await queryRunner.query(`ALTER TABLE "temporary_banner" RENAME TO "banner"`);
@@ -13,7 +14,7 @@ export class Initialize1543364915973 implements MigrationInterface {
 
     public async down(queryRunner: QueryRunner): Promise<any> {
         await queryRunner.query(`ALTER TABLE "banner" RENAME TO "temporary_banner"`);
-        await queryRunner.query(`CREATE TABLE "banner" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "source" varchar NOT NULL, "description" varchar NOT NULL, "topicId" integer)`);
+        await queryRunner.query(`CREATE TABLE "banner" ("id" integer PRIMARY KEY ${autoIncrement} NOT NULL, "source" varchar NOT NULL, "description" varchar NOT NULL, "topicId" integer)`);
         await queryRunner.query(`INSERT INTO "banner"("id", "source", "description", "topicId") SELECT "id", "source", "description", "topicId" FROM "temporary_banner"`);
         await queryRunner.query(`DROP TABLE "temporary_banner"`);
         await queryRunner.query(`DROP TABLE "banner"`);
